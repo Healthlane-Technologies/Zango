b5b92e67828fee0131f0c1fbb29ddf23d733a02d
Harsh Shah <shahharsh176@gmail.com>
2023-10-11 22:18:59 +0530

    add: app landing module, app panel fixes

--- backend/src/zelthy/apps/appauth/urls.py
+++ backend/src/zelthy/apps/appauth/urls.py
@@ -1,7 +1,7 @@
 """Defines which API URLs."""
 from django.urls import re_path
 
-from .views import AppUserLoginView, AppUserLandingView, AppLogoutView
+from .views import AppUserLoginView, AppUserLandingView, AppLogoutView, SwitchUserRoleView
 
 urlpatterns = [
     re_path(r"^login/", AppUserLoginView.as_view(), name="app-login-view"),
@@ -9,6 +9,11 @@ urlpatterns = [
         r"^user-landing/",
         AppUserLandingView.as_view(),
         name="appuser-landing-view",
+    ),
+    re_path(
+        r"^switch_role/(?P<role_id>\d+)/$",
+        SwitchUserRoleView.as_view(),
+        name="appuser-switch-role-view",
     ),
     re_path(r"^logout/", view=AppLogoutView.as_view(), name="app-logout-view"),
 ]
