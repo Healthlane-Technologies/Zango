d7fd2179e27a479b54631c7188b49d6f529ef28b
deepakdinesh1123 <d.deepakdinesh13@gmail.com>
2023-10-13 12:28:55 +0530

    sync_static fixed

--- backend/src/zelthy/apps/shared/tenancy/management/commands/ws_makemigration.py
+++ backend/src/zelthy/apps/shared/tenancy/management/commands/ws_makemigration.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.db import connection
 
-from django.core.management.commands.makemigrations import Command \
-    as MakeMigrationsCommand
+from django.core.management.commands.makemigrations import (
+    Command as MakeMigrationsCommand,
+)
 from zelthy.apps.shared.tenancy.models import TenantModel
 from zelthy.apps.dynamic_models.workspace.base import Workspace
 
@@ -17,8 +18,8 @@ class Command(MakeMigrationsCommand):
 
     Known issues:
         - Migrations generated for relational tables where the referred table
-        is not migrated already causes an issue in the migration. In such 
-        cases, the migration file has to be manually edited to move the 
+        is not migrated already causes an issue in the migration. In such
+        cases, the migration file has to be manually edited to move the
         relational field out of the CreateModel block to a ModifyField block.
     """
 
@@ -31,22 +32,21 @@ class Command(MakeMigrationsCommand):
             help="The workspace name to be used.",
         )
         parser.add_argument(
-            "--test",
-            action='store_true',
-            help="Run the migration for test database"
+            "--test", action="store_true", help="Run the migration for test database"
         )
 
     def handle(self, *app_labels, **options):
-        is_test_mode = options['test']
-        tenant = options['workspace']
+        is_test_mode = options["test"]
+        tenant = options["workspace"]
         tenant_obj = TenantModel.objects.get(name=tenant)
         connection.set_tenant(tenant_obj)
         if is_test_mode:
-            connection.settings_dict['NAME'] = "test_"+ \
-                    connection.settings_dict['NAME']
-        settings.MIGRATION_MODULES = { 
-            f'dynamic_models': f"workspaces.{options['workspace']}.dmigrations" 
+            connection.settings_dict["NAME"] = (
+                "test_" + connection.settings_dict["NAME"]
+            )
+        settings.MIGRATION_MODULES = {
+            f"dynamic_models": f"workspaces.{options['workspace']}.dmigrations"
         }
-        w = Workspace(tenant_obj, None,  True)
-        w.load_models()
-        super().handle('dynamic_models', **options)
+        w = Workspace(tenant_obj, None, True)
+        w.load_models(migration=True)
+        super().handle("dynamic_models", **options)
