a1bf2d19f34ab70e8c9db529a2425f7777742b93
Kaustubh <kaustubh@zelthy.com>
2023-10-24 08:00:30 +0530

    internal routing

--- backend/src/zelthy/core/utils.py
+++ backend/src/zelthy/core/utils.py
@@ -10,3 +10,14 @@ def get_current_role():
     # model = apps.get_model('appauth', 'UserRoleModel')
     return getattr(_request_local, 'user_role', None)
     # return model.objects.get(id=user_role_id)
+
+
+def get_package_url(tenant, package_name, path):
+    """
+        returns the root path of the package 
+    """
+    ws_klass = getattr(import_module('zelthy.apps.dynamic_models.workspace.base'), 'Workspace')
+    ws = ws_klass(tenant)
+    ws_settings = ws.get_workspace_settings()
+    plugin_root_path = ws_settings["plugin_routes"][package_name]
+    return f"http://{domain_url}/{plugin_root_path}/{path}" #this is used for internal routing so http should suffice
