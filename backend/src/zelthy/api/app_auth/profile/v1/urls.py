3e37a97f66359eb619d8808be5cd0bd435cf8e95
deepakdinesh1123 <d.deepakdinesh13@gmail.com>
2023-11-03 16:49:13 +0530

    profile password change api refactored

--- backend/src/zelthy/api/app_auth/profile/v1/urls.py
+++ backend/src/zelthy/api/app_auth/profile/v1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
 
-from .views import ProfileViewAPIV1
+from .views import ProfileViewAPIV1, PasswordChangeViewAPIV1
 
 urlpatterns = [
     re_path(r"", ProfileViewAPIV1.as_view(), name="profile"),
+    re_path(r"change_password", PasswordChangeViewAPIV1.as_view(), name="change_password"),
 ]
