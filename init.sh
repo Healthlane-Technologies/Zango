b1c660cc4f1314ea0820a03e40f5e6c36bae5b18
deepakdinesh1123 <d.deepakdinesh13@gmail.com>
2023-12-13 16:07:03 +0530

    volume mount added to prod

--- init.sh
+++ init.sh
@@ -10,16 +10,12 @@ if [ "$ENV" = "dev" ]; then
     python manage.py runserver 0.0.0.0:8000
 else
     if [ -d "$PROJECT_NAME" ]; then
-        cd "$PROJECT_NAME"
-        rm -rf workspaces
-        mkdir workspaces
+        echo "restarting existing project"
     else
         zelthy3 start-project $PROJECT_NAME --db_name="$POSTGRES_DB" --db_user="$POSTGRES_USER" --db_password="$POSTGRES_PASSWORD" --db_host="$POSTGRES_HOST" --db_port="$POSTGRES_PORT" --platform_username="$PLATFORM_USERNAME" --platform_user_password="$PLATFORM_USER_PASSWORD"
-        cd "$PROJECT_NAME"
-        mkdir workspaces
     fi
-    mv /zelthy/temp_workspace/* workspaces
-    mv /zelthy/temp_config/gunicorn.conf.py .
+    cd "$PROJECT_NAME"
+    mv /zelthy/config/gunicorn.conf.py .
     python manage.py collectstatic --noinput
     python manage.py migrate_schemas
     gunicorn -c gunicorn.conf.py "$PROJECT_NAME".wsgi
--- prod.dockerfile
+++ prod.dockerfile
@@ -1,8 +1,5 @@
 FROM zelthy3:latest
 
 RUN pip install gunicorn
-COPY ${PROJECT_NAME}/workspaces/ /zelthy/temp_workspace/
-COPY init.sh /zelthy/
-COPY config/gunicorn.conf.py /zelthy/temp_config/
 WORKDIR /zelthy/
 CMD ["/bin/sh", "init.sh"]
