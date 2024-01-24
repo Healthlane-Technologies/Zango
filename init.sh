1b4dbdfe70fb1cc0808f9680444bb946a517100c
deepakdinesh1123 <d.deepakdinesh13@gmail.com>
2024-01-24 14:17:41 +0530

    celery service added

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ if [ "$ENV" = "dev" ]; then
     if [ -d "$PROJECT_NAME" ]; then
         echo "restarting existing project"
     else
-        zelthy3 start-project $PROJECT_NAME --db_name="$POSTGRES_DB" --db_user="$POSTGRES_USER" --db_password="$POSTGRES_PASSWORD" --db_host="$POSTGRES_HOST" --db_port="$POSTGRES_PORT" --platform_username="$PLATFORM_USERNAME" --platform_user_password="$PLATFORM_USER_PASSWORD"
+        zelthy3 start-project $PROJECT_NAME --db_name="$POSTGRES_DB" --db_user="$POSTGRES_USER" --db_password="$POSTGRES_PASSWORD" --db_host="$POSTGRES_HOST" --db_port="$POSTGRES_PORT" --platform_username="$PLATFORM_USERNAME" --platform_user_password="$PLATFORM_USER_PASSWORD" --redis_host="$REDIS_HOST"
     fi
     cd "$PROJECT_NAME"
     python manage.py runserver 0.0.0.0:8000
@@ -12,7 +12,7 @@ else
     if [ -d "$PROJECT_NAME" ]; then
         echo "restarting existing project"
     else
-        zelthy3 start-project $PROJECT_NAME --db_name="$POSTGRES_DB" --db_user="$POSTGRES_USER" --db_password="$POSTGRES_PASSWORD" --db_host="$POSTGRES_HOST" --db_port="$POSTGRES_PORT" --platform_username="$PLATFORM_USERNAME" --platform_user_password="$PLATFORM_USER_PASSWORD"
+        zelthy3 start-project $PROJECT_NAME --db_name="$POSTGRES_DB" --db_user="$POSTGRES_USER" --db_password="$POSTGRES_PASSWORD" --db_host="$POSTGRES_HOST" --db_port="$POSTGRES_PORT" --platform_username="$PLATFORM_USERNAME" --platform_user_password="$PLATFORM_USER_PASSWORD" --redis_host="$REDIS_HOST"
     fi
     cd "$PROJECT_NAME"
     mv /zelthy/config/gunicorn.conf.py .
