7cca8bf7a67437856e97ac2d3ffa970388428875
aswin-a-z <aswin@zelthy.com>
2024-02-09 17:28:54 +0530

    Few cleanups and production changes

--- docs/src/pages/login.js
+++ docs/src/pages/login.js
@@ -8,8 +8,8 @@ export default function Home() {
 
   return (
     <Layout
-      title={`Zelthy for Devs`}
-      description="Zelthy for Devs">
+      title={`Zelthy Docs`}
+      description="Zelthy Docs">
       <main>
       <iframe
         style={{height: '100vh'}}
