1b1aca8cd344dcb4bd0b1cf1795f42d381f64f6a
Deepak Suresh <88372489+deepaksuresh2411@users.noreply.github.com>
2024-02-13 12:37:55 +0530

    Update views.py

--- backend/src/zelthy/apps/dynamic_models/views.py
+++ backend/src/zelthy/apps/dynamic_models/views.py
@@ -57,8 +57,6 @@ class DynamicView(View, PermMixin):
                     return HttpResponseForbidden(
                         "You don't have permission to view this page"
                     )
-            if request.path == '/':
-                 return redirect('/login')
              
             return Http404()
         return HttpResponseForbidden("You don't have permission to view this page")
