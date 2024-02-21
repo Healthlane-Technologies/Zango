d53a75aa6834e862d19f95a70ae775a1a8813067
deepakdinesh1123 <d.deepakdinesh13@gmail.com>
2024-02-21 10:46:48 +0530

    app creation result stored and response structure changed

--- backend/src/zelthy/apps/shared/tenancy/tasks.py
+++ backend/src/zelthy/apps/shared/tenancy/tasks.py
@@ -11,46 +11,55 @@ from .utils import assign_policies_to_anonymous_user, DEFAULT_THEME_CONFIG
 
 @shared_task
 def initialize_workspace(tenant_uuid):
-    from zelthy.apps.shared.tenancy.models import TenantModel, ThemesModel
+    try:
+        from zelthy.apps.shared.tenancy.models import TenantModel, ThemesModel
 
+        tenant = TenantModel.objects.get(uuid=tenant_uuid)
 
-    tenant = TenantModel.objects.get(uuid=tenant_uuid)
+        # Creating schema
+        tenant.create_schema(check_if_exists=True)
 
-    # Creating schema
-    tenant.create_schema(check_if_exists=True)
+        # migrating schema
+        call_command(
+            "migrate_schemas",
+            tenant=True,
+            schema_name=tenant.schema_name,
+            interactive=False,
+        )
 
-    # migrating schema
-    call_command(
-        "migrate_schemas",
-        tenant=True,
-        schema_name=tenant.schema_name,
-        interactive=False,
-    )
+        # Create workspace Folder
+        project_base_dir = settings.BASE_DIR
 
-    # Create workspace Folder
-    project_base_dir = settings.BASE_DIR
+        workspace_dir = os.path.join(project_base_dir, "workspaces")
+        if not os.path.exists(workspace_dir):
+            os.makedirs(workspace_dir)
 
-    workspace_dir = os.path.join(project_base_dir, "workspaces")
-    if not os.path.exists(workspace_dir):
-        os.makedirs(workspace_dir)
+        # Creating app folder with the initial files
+        template_directory = os.path.join(
+            os.path.dirname(__file__), "workspace_folder_template"
+        )
+        cookiecutter_context = {"app_name": tenant.name}
 
-    # Creating app folder with the initial files
-    template_directory = os.path.join(
-        os.path.dirname(__file__), "workspace_folder_template"
-    )
-    cookiecutter_context = {"app_name": tenant.name}
+        cookiecutter.main.cookiecutter(
+            template_directory,
+            extra_context=cookiecutter_context,
+            output_dir=workspace_dir,
+            no_input=True,
+        )
 
-    cookiecutter.main.cookiecutter(
-        template_directory,
-        extra_context=cookiecutter_context,
-        output_dir=workspace_dir,
-        no_input=True,
-    )
+        tenant.status = "deployed"
+        tenant.save()
 
-    tenant.status = "deployed"
-    tenant.save()
-
-    assign_policies_to_anonymous_user(tenant.schema_name)
-    theme = ThemesModel.objects.create(
+        assign_policies_to_anonymous_user(tenant.schema_name)
+        theme = ThemesModel.objects.create(
             name="Default", tenant=tenant, config=DEFAULT_THEME_CONFIG
         )
+        if tenant.status == "deployed":
+            return {"result": "success"}
+        else:
+            return {"result": "failure", "error": "Failed, see error logs"}
+    except Exception as e:
+        return {
+            "result": "failure",
+            "error": str(e),
+        }
