7a9d6aea383d7c4b927d87ee33e530c329ac4088
Rajat bubna <72187837+rajatb2814@users.noreply.github.com>
2024-02-22 17:51:12 +0530

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: "https://zelthystatichosting.s3.ap-south-1.amazonaws.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "docs/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
