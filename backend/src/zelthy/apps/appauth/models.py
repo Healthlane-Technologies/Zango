c9ee814f885d06c9b48ec3eb18de529ead67b4a9
deepaksuresh2411 <deepak.suresh@zelthy.com>
2024-03-11 18:51:16 +0530

    wip: appuser model changes

--- backend/src/zelthy/apps/appauth/models.py
+++ backend/src/zelthy/apps/appauth/models.py
@@ -67,6 +67,16 @@ class AppUserModel(AbstractZelthyUserModel, PermissionMixin):
         return self.name
 
     def is_user_active(self, role_name=None):
+        """
+        Check if the user is active.
+
+        Parameters:
+        - role_name (str): The name of the role to check for. If provided, the method will check if the user has the specified role and if both the role and the user are active.
+
+        Returns:
+        - bool: True if the user is active and, if role_name is provided, the role is active as well. False otherwise.
+
+        """
         if role_name:
             user_role_mapping = self._get_role_mapping(role_name)
             if user_role_mapping:
@@ -77,7 +87,9 @@ class AppUserModel(AbstractZelthyUserModel, PermissionMixin):
     @property
     def roles(self):
         roles = UserRoleModel.objects.none()
-        for user_role in AppUserRoleMappingModel.objects.filter(user = self, is_active = True).only('role'):
+        for user_role in AppUserRoleMappingModel.objects.filter(
+            user=self, is_active=True
+        ).only("role"):
             roles |= UserRoleModel.objects.filter(pk=user_role.role.pk)
         return roles
 
@@ -87,7 +99,6 @@ class AppUserModel(AbstractZelthyUserModel, PermissionMixin):
             if object_uuid:
                 return ObjectStore.get_object(object_uuid)
         return None
-
 
     def has_perm(self, request, perm_type, view=None, dataModel=None):
         if perm_type == "userAccess":
@@ -115,25 +126,54 @@ class AppUserModel(AbstractZelthyUserModel, PermissionMixin):
         return False
 
     def add_roles(self, role_ids):
+        """
+        Add roles to the user.
+
+        Parameters:
+        - role_ids (list): A list of role IDs to be added to the user.
+
+        Returns:
+        - None
+
+        """
         roles = UserRoleModel.objects.filter(id__in=role_ids)
-        assigned_roles = AppUserRoleMappingModel.objects.filter(user = self)
+        assigned_roles = AppUserRoleMappingModel.objects.filter(user=self)
         for role in roles:
-            if assigned_roles.filter(role = role).exists():
-                assigned_roles.filter(role = role).update(is_active = True)
+            if assigned_roles.filter(role=role).exists():
+                assigned_roles.filter(role=role).update(is_active=True)
             else:
-                AppUserRoleMappingModel.objects.create(user = self, role=role)
+                AppUserRoleMappingModel.objects.create(user=self, role=role)
 
     def remove_roles(self, role_ids):
+        """
+        Remove roles from the user.
+
+        Parameters:
+        - role_ids (list): A list of role IDs to be removed from the user.
+
+        Returns:
+        - None
+        """
         roles = UserRoleModel.objects.filter(id__in=role_ids)
         for role in roles:
             AppUserRoleMappingModel.objects.filter(user=self, role=role).delete()
-    
+
     def update_roles(self, role_ids):
+        """
+        Update the roles of the user by deleting all existing roles and adding new ones.
+        This effectively replaces the user's roles with the new set of roles provided.
+
+        Parameters:
+        - role_ids (list): A list of role IDs to be assigned to the user. These IDs replace any existing roles the user has.
+
+        Returns:
+        - None
+        """
+
         AppUserRoleMappingModel.objects.filter(user=self).delete()
         roles = UserRoleModel.objects.filter(id__in=role_ids)
         for role in roles:
-            AppUserRoleMappingModel.objects.create(user = self, role=role)
-            
+            AppUserRoleMappingModel.objects.create(user=self, role=role)
 
     def check_password_validity(self, password):
         """
@@ -158,7 +198,7 @@ class AppUserModel(AbstractZelthyUserModel, PermissionMixin):
         role_ids=[],
         force_password_reset=True,
         require_verification=True,
-        app_objects=None
+        app_objects=None,
     ):
         """ """
         success = False
@@ -198,8 +238,8 @@ class AppUserModel(AbstractZelthyUserModel, PermissionMixin):
                         if password:
                             app_user.set_password(password)
                         else:
-                            app_user.set_unusable_password() # Ask Rajat
-                            
+                            app_user.set_unusable_password()  # Ask Rajat
+
                         if require_verification:
                             app_user.is_active = False
                         else:
@@ -218,7 +258,7 @@ class AppUserModel(AbstractZelthyUserModel, PermissionMixin):
                         message = "App User created successfully."
             except Exception as e:
                 message = str(e)
-        return {"success": success, "message": message, 'app_user': app_user}
+        return {"success": success, "message": message, "app_user": app_user}
 
     def update_user(self, data):
         success = False
@@ -303,31 +343,70 @@ class AppUserModel(AbstractZelthyUserModel, PermissionMixin):
             pass
 
     def has_role(self, role_name):
+        """
+        Checks if the user has a specific role.
+
+        Parameters:
+        - role_name (str): The name of the role to check for.
+
+        Returns:
+        - bool: True if the user has the specified role, False otherwise.
+        """
         user_role_mapping = self._get_role_mapping(role_name=role_name)
         return True if user_role_mapping and user_role_mapping.is_active else False
 
     def activate(self, role_name=None):
+        """
+        Activate the user or a specific role.
+
+        Parameters:
+        - role_name (str, optional): The name of the role to activate. If provided, only the specified role will be activated. If not provided, user will be activated.
+
+        Returns:
+        - None
+
+        """
         if role_name:
             user_role_mapping = self._get_role_mapping(role_name)
             if user_role_mapping:
                 user_role_mapping.is_active = True
-                user_role_mapping.save()
+                user_role_mapping.save(update_fields=["is_active"])
         else:
             self.is_active = True
-            self.save()
-    
+            self.save(update_fields=["is_active"])
+
     def deactivate(self, role_name=None):
+        """
+        Deactivates the user or a specific role.
+
+        Parameters:
+        - role_name (str, optional): The name of the role to deactivate. If provided, only the specified role will be deactivated. If not provided, user will be deactivated.
+
+        Returns:
+        - None
+
+        """
         if role_name:
             user_role_mapping = self._get_role_mapping(role_name)
             if user_role_mapping:
                 user_role_mapping.is_active = False
-                user_role_mapping.save()
+                user_role_mapping.save(update_fields=["is_active"])
         else:
             self.is_active = False
-            self.save()
+            self.save(update_fields=["is_active"])
 
     @classmethod
     def filter_users(cls, query):
+        """
+        Filter users based on the provided query parameters.
+
+        Parameters:
+        - query (dict): A dictionary containing the query parameters to filter the users. The keys of the dictionary should correspond to the field names of the User model, and the values should be the desired values for those fields.
+
+        Returns:
+        - QuerySet: A QuerySet containing the filtered users.
+
+        """
         return cls.objects.filter(**query)
 
 
@@ -335,9 +414,12 @@ class OldPasswords(AbstractOldPasswords):
     user = models.ForeignKey(AppUserModel, on_delete=models.PROTECT)
 
 
-class AppUserRoleMappingModel(models.Model):
+class AppUserRoleMappingModel(FullAuditMixin):
 
-    user = models.ForeignKey(AppUserModel, related_name='app_user', on_delete=models.CASCADE)
-    role = models.ForeignKey(UserRoleModel, related_name='app_user_role', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        AppUserModel, related_name="app_user", on_delete=models.CASCADE
+    )
+    role = models.ForeignKey(
+        UserRoleModel, related_name="app_user_role", on_delete=models.CASCADE
+    )
     is_active = models.BooleanField(default=True)
-
