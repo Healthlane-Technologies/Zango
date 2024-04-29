12c22f12ec3a335846bf66268b8f99b69b761386
deepakdinesh1123 <52698821+deepakdinesh1123@users.noreply.github.com>
2024-04-29 17:39:32 +0530

    get_package_url method fixed and production docker compose fixed (#163)
    * get_package_url method fixed and production docker compose fixed
    
    * request meta error fixed and nice requirements added
    
    * fix: remove pydub duplicate requirement
    
    * method to create mock request if request is not available added
    
    ---------
    
    Co-authored-by: Harsh Shah <shahharsh176@gmail.com>

--- backend/src/zelthy/core/utils.py
+++ backend/src/zelthy/core/utils.py
@@ -27,6 +27,8 @@ def get_app_object():
 
 
 def get_package_url(request, path, package_name):
+    if not request:
+        request = get_mock_request()
     with open(f"workspaces/{request.tenant.name}/settings.json", "r") as f:
         data = json.loads(f.read())
     for route in data["package_routes"]:
@@ -37,19 +39,34 @@ def get_package_url(request, path, package_name):
     return ""
 
 
+def get_mock_request(**kwargs):
+    from django.db import connection
+    from django.http import HttpRequest
+
+    request = HttpRequest()
+    request.path = kwargs.get("path")
+    request.tenant = connection.tenant
+    request.method = kwargs.get("method", "GET")
+    request.META = kwargs.get("META", {})
+    request.header = kwargs.get("header", {})
+
+    return request
+
 def get_current_request_url(request, domain=None):
     # Determine the protocol (HTTP or HTTPS) based on the request's is_secure() method.
-    if settings.ENV == "dev":
-        protocol = "https" if request.is_secure() else "http"
-    else:
+    secure = (
+        request.headers.get("X-Forwarded-Proto") == "https"
+        or request.META.get("HTTP_X_FORWARDED_PROTO") == "https"
+    )
+    if secure:
         protocol = "https"
+    else:
+        protocol = "http"
 
     # Get the hostname (domain) from the request.
     if not domain:
         domain = request.get_host()
-
-    # Get the port from the request. Use the standard ports (80 for HTTP, 443 for HTTPS) as default.
-    if settings.ENV == "dev":
+    if not secure:
         port = request.META.get("SERVER_PORT", "")
         if (protocol == "http" and port == "80") or (
             protocol == "https" and port == "443"
--- deploy/prod.dockerfile
+++ deploy/prod.dockerfile
@@ -1,5 +1,22 @@
 FROM kczelthy/zelthy3:latest
 
 RUN pip install gunicorn
+RUN apt-get install net-tools
+RUN apt install ffmpeg -y
+
+ARG UID
+ARG GID
+
+# Update the package list, install sudo, create a non-root user, and grant password-less sudo permissions
+RUN apt update && \
+    apt install -y sudo && \
+    groupadd -o -g ${GID} -r zelthy_user && \
+    adduser --uid $UID --gid $GID --disabled-password --gecos "" zelthy_user && \
+    echo 'zelthy_user ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+
+# Set the non-root user as the default user
+USER zelthy_user
+
+COPY init.sh /zelthy/
 WORKDIR /zelthy/
 CMD ["/bin/sh", "init.sh"]
