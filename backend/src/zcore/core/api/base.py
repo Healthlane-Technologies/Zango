00f2aaf0ea6648c3e1850d41b80301751324bbf2
Harsh Shah <shahharsh176@gmail.com>
2024-05-08 17:18:38 +0530

    change docstring

--- backend/src/zcore/core/api/base.py
+++ backend/src/zcore/core/api/base.py
@@ -5,30 +5,28 @@ from knox.auth import TokenAuthentication
 from ..permissions import IsAuthenticatedPlatformUser, IsAuthenticatedAppUser
 
 
-
 class ZCoreSessionPlatformAPIView(APIView):
     """
     This is the base class for developing Platform APIs that need to support only session based
     authentication. Expected use cases include APIs that must be supported only with session and NOT token
     """
 
-    authentication_classes = (SessionAuthentication, )
+    authentication_classes = (SessionAuthentication,)
     permission_classes = (IsAuthenticatedPlatformUser,)
-    
+
 
 class ZCoreSessionAppAPIView(APIView):
     """
     Base API view for developing Session Authenticated Platform APIs
     Use Case: Platform APIs accessed by the web apps on the same domain
     CSRF Not Exempted
-    This API expects ZCoreCookie & csrftoken as Cookies. ZCoreCookie should 
+    This API expects zcorecookie & csrftoken as Cookies. zcorecookie should
     represent an active platform user.
     Additional Perms: Request should pass whitelisting setting of Platform
     """
 
-    authentication_classes = (SessionAuthentication, )
+    authentication_classes = (SessionAuthentication,)
     permission_classes = (IsAuthenticatedAppUser,)
-
 
 
 class ZCoreTokenPlatformAPIView(APIView):
@@ -37,10 +35,8 @@ class ZCoreTokenPlatformAPIView(APIView):
     authentication. Expected use cases include APIs that will be used by third parties only
     """
 
-    authentication_classes = (TokenAuthentication, )
+    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticatedPlatformUser,)
-
-
 
 
 class ZCoreGenericPlatformAPIView(APIView):
@@ -50,11 +46,10 @@ class ZCoreGenericPlatformAPIView(APIView):
     The ZCore platform webapp must use session based authentication and should provide csrftoken as well.
     """
 
-    authentication_classes = (SessionAuthentication,
-                                TokenAuthentication)
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
     permission_classes = (IsAuthenticatedPlatformUser,)
 
-class ZCoreGenericAppAPIView(APIView):
 
+class ZCoreGenericAppAPIView(APIView):
     authentication_classes = (TokenAuthentication, SessionAuthentication)
     permission_classes = (IsAuthenticatedAppUser,)
