92391b811f2835d184e2413733f08cffdcb24074
deepakdinesh1123 <d.deepakdinesh13@gmail.com>
2024-05-13 15:38:41 +0530

    unsued views cleaned up

--- backend/src/zcore/apps/shared/platformauth/views.py
+++ backend/src/zcore/apps/shared/platformauth/views.py
@@ -15,24 +15,3 @@ class PlatformUserLoginView(LoginView):
     def post(self, request, *args, **kwargs):
         super().post(request, *args, **kwargs)
         return redirect("/platform")
-
-
-class PlatformUserLoginAPIV1(APIView):
-    pass
-
-
-class PlatformUserProfileAPIV1(ZCoreSessionPlatformAPIView):
-    """ """
-
-    def get_profile_data(self, request):
-        data = {}
-        data["name"] = request.user.platform_user.name
-        data["email"] = request.user.platform_user.email
-        data["mobile"] = request.user.platform_user.mobile
-        data["role"] = "Platform User"
-        data["user_uuid"] = request.user.username
-        return data
-
-    def get(self, request, *args, **kwargs):
-        data = self.get_profile_data(request)
-        return get_api_response(True, data, 200)
