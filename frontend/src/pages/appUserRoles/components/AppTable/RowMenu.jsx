1d2c63fb32b555d70711c7776c5f986a06015fe5
Madhu <madhu@zelthy.com>
2024-06-24 22:03:53 +0530

    locator_updates

--- frontend/src/pages/appUserRoles/components/AppTable/RowMenu.jsx
+++ frontend/src/pages/appUserRoles/components/AppTable/RowMenu.jsx
@@ -42,6 +42,7 @@ export default function RowMenu({ rowData }) {
 		<Menu as="div" className="relative flex">
 			<Menu.Button
 				data-cy="three_dots_menu"
+				data-cy="three_dots_menu"
 				className="flex w-full justify-center focus:outline-none"
 				ref={(ref) => setReferenceElement(ref)}
 			>
@@ -56,55 +57,53 @@ export default function RowMenu({ rowData }) {
 			>
 				<Menu.Items className="absolute right-0 top-[30px] w-[186px] origin-top-right rounded-[4px] bg-white shadow-table-menu focus:outline-none">
 					<div className="p-[4px]">
+						<Menu.Item>
+							{({ active }) => (
+								<button
+									data-cy="edit_user_role_button"
+									type="button"
+									className="flex w-full"
+									onClick={handleEditUserDetails}
+								>
+									<div
+										className={`${
+											active ? 'bg-[#F0F3F4]' : ''
+										} flex w-full flex-col rounded-[2px] px-[12px] py-[8px]`}
+									>
+										<span className="text-start font-lato text-[14px] font-bold leading-[20px] tracking-[0.2px] text-[#212429]">
+											Edit User Role
+										</span>
+										<span className="text-start font-lato text-[12px] leading-[16px] tracking-[0.2px] text-[#6C747D]">
+											user role name, policy
+										</span>
+									</div>
+								</button>
+							)}
+						</Menu.Item>
 						{rowData?.is_active ? (
-							<>
-								<Menu.Item>
-									{({ active }) => (
-										<button
-											data-cy="edit_user_role_button"
-											type="button"
-											className="flex w-full"
-											onClick={handleEditUserDetails}
+							<Menu.Item>
+								{({ active }) => (
+									<button
+										data-cy="deactivate_user_role_button"
+										type="button"
+										className="flex  w-full"
+										onClick={handleDeactivateUser}
+									>
+										<div
+											className={`${
+												active ? 'bg-[#F0F3F4]' : ''
+											} flex w-full flex-col rounded-[2px] px-[12px] py-[8px]`}
 										>
-											<div
-												className={`${
-													active ? 'bg-[#F0F3F4]' : ''
-												} flex w-full flex-col rounded-[2px] px-[12px] py-[8px]`}
-											>
-												<span className="text-start font-lato text-[14px] font-bold leading-[20px] tracking-[0.2px] text-[#212429]">
-													Edit User Role
-												</span>
-												<span className="text-start font-lato text-[12px] leading-[16px] tracking-[0.2px] text-[#6C747D]">
-													user role name, policy
-												</span>
-											</div>
-										</button>
-									)}
-								</Menu.Item>
-								<Menu.Item>
-									{({ active }) => (
-										<button
-											data-cy="deactivate_user_role_button"
-											type="button"
-											className="flex  w-full"
-											onClick={handleDeactivateUser}
-										>
-											<div
-												className={`${
-													active ? 'bg-[#F0F3F4]' : ''
-												} flex w-full flex-col rounded-[2px] px-[12px] py-[8px]`}
-											>
-												<span className="text-start font-lato text-[14px] font-bold leading-[20px] tracking-[0.2px] text-[#AA2113]">
-													Deactivate User Role
-												</span>
-												<span className="text-start font-lato text-[12px] leading-[16px] tracking-[0.2px] text-[#6C747D]">
-													this will not delete the user role but only disable it
-												</span>
-											</div>
-										</button>
-									)}
-								</Menu.Item>
-							</>
+											<span className="text-start font-lato text-[14px] font-bold leading-[20px] tracking-[0.2px] text-[#AA2113]">
+												Deactivate User Role
+											</span>
+											<span className="text-start font-lato text-[12px] leading-[16px] tracking-[0.2px] text-[#6C747D]">
+												this will not delete the user role but only disable it
+											</span>
+										</div>
+									</button>
+								)}
+							</Menu.Item>
 						) : (
 							<Menu.Item>
 								{({ active }) => (
