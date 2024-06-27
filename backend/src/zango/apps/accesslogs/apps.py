1af88b26a7e57cb6d0ddfd16e33baa212b7d059e
deepaksuresh2411 <deepak@zelthy.com>
2024-06-27 11:59:12 +0530

    feat(accesslogs): zango related changes

---
+++ backend/src/zango/apps/accesslogs/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class AccesslogsAppConfig(AppConfig):
+
+    name = "zango.apps.accesslogs"
+
+    def ready(self):
+        import zango.apps.accesslogs.signals
