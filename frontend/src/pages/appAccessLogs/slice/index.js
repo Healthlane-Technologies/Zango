8f592fc00af4a40fbd190a2caa62865c5b2a8fd5
Ravi <ravi@zelthy.com>
2024-07-16 16:10:04 +0530

    fe accesslogs code clean up

--- frontend/src/pages/appAccessLogs/slice/index.js
+++ frontend/src/pages/appAccessLogs/slice/index.js
@@ -3,10 +3,6 @@ import { createSlice } from '@reduxjs/toolkit';
 export const appAccessLogsSlice = createSlice({
 	name: 'appAccessLogs',
 	initialState: {
-		isAddNewUserRolesModalOpen: false,
-		isEditUserRolesDetailModalOpen: false,
-		isDeactivateUserRolesModalOpen: false,
-		isActivateUserRolesModalOpen: false,
 		rerenderPage: false,
 		appAccessLogsData: null,
 		appAccessLogsFormData: null,
@@ -19,52 +15,6 @@ export const appAccessLogsSlice = createSlice({
 		isAppAccessLogsDataEmpty: true,
 	},
 	reducers: {
-		toggleIsAddNewUserRolesModalOpen: (state) => {
-			state.isAddNewUserRolesModalOpen = !state.isAddNewUserRolesModalOpen;
-		},
-		openIsAddNewUserRolesModalOpen: (state, action) => {
-			state.isAddNewUserRolesModalOpen = true;
-			state.appAccessLogsFormData = action.payload;
-		},
-		closeIsAddNewUserRolesModalOpen: (state, action) => {
-			state.isAddNewUserRolesModalOpen = false;
-			state.appAccessLogsFormData = null;
-		},
-		toggleIsEditUserRolesDetailModalOpen: (state) => {
-			state.isEditUserRolesDetailModalOpen =
-				!state.isEditUserRolesDetailModalOpen;
-		},
-		openIsEditUserRolesDetailModalOpen: (state, action) => {
-			state.isEditUserRolesDetailModalOpen = true;
-			state.appAccessLogsFormData = action.payload;
-		},
-		closeIsEditUserRolesDetailModalOpen: (state) => {
-			state.isEditUserRolesDetailModalOpen = false;
-			state.appAccessLogsFormData = null;
-		},
-		toggleIsDeactivateUserRolesModalOpen: (state) => {
-			state.isDeactivateUserRolesModalOpen =
-				!state.isDeactivateUserRolesModalOpen;
-		},
-		openIsDeactivateUserRolesModalOpen: (state, action) => {
-			state.isDeactivateUserRolesModalOpen = true;
-			state.appAccessLogsFormData = action.payload;
-		},
-		closeIsDeactivateUserRolesModalOpen: (state) => {
-			state.isDeactivateUserRolesModalOpen = false;
-			state.appAccessLogsFormData = null;
-		},
-		toggleIsActivateUserRolesModalOpen: (state) => {
-			state.isActivateUserRolesModalOpen = !state.isActivateUserRolesModalOpen;
-		},
-		openIsActivateUserRolesModalOpen: (state, action) => {
-			state.isActivateUserRolesModalOpen = true;
-			state.appAccessLogsFormData = action.payload;
-		},
-		closeIsActivateUserRolesModalOpen: (state) => {
-			state.isActivateUserRolesModalOpen = false;
-			state.appAccessLogsFormData = null;
-		},
 		setAppAccessLogsData: (state, action) => {
 			state.appAccessLogsData = action.payload;
 			state.isAppAccessLogsDataEmpty =
@@ -84,34 +34,14 @@ export const appAccessLogsSlice = createSlice({
 });
 
 export const {
-	toggleIsAddNewUserRolesModalOpen,
-	openIsAddNewUserRolesModalOpen,
-	closeIsAddNewUserRolesModalOpen,
-	toggleIsEditUserRolesDetailModalOpen,
-	openIsEditUserRolesDetailModalOpen,
-	closeIsEditUserRolesDetailModalOpen,
-	toggleIsDeactivateUserRolesModalOpen,
-	openIsDeactivateUserRolesModalOpen,
-	closeIsDeactivateUserRolesModalOpen,
-	toggleIsActivateUserRolesModalOpen,
-	openIsActivateUserRolesModalOpen,
-	closeIsActivateUserRolesModalOpen,
 	setAppAccessLogsData,
 	setAppAccessLogsTableData,
 	toggleRerenderPage,
 } = appAccessLogsSlice.actions;
 
-export const selectIsAddNewUserRolesModalOpen = (state) =>
-	state.appAccessLogs.isAddNewUserRolesModalOpen;
-
-export const selectIsEditUserRolesDetailModalOpen = (state) =>
-	state.appAccessLogs.isEditUserRolesDetailModalOpen;
 
 export const selectIsDeactivateUserRolesModalOpen = (state) =>
 	state.appAccessLogs.isDeactivateUserRolesModalOpen;
-
-export const selectIsActivateUserRolesModalOpen = (state) =>
-	state.appAccessLogs.isActivateUserRolesModalOpen;
 
 export const selectRerenderPage = (state) => state.appAccessLogs.rerenderPage;
 
