32c3f7ed212869fe9fbd9187eed94513b12c6789
Ravi <ravi@zelthy.com>
2024-07-23 13:03:09 +0530

    build script update for workflow

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 	},
 	output: {
 		path: path.resolve(__dirname, '../backend/src/zango/assets/app_panel/js'),
-		filename: `build.v${metadata.buildMajor}.${metadata.buildMinor}.${metadata.buildPatch}.min.js`,
+		filename: `build.${Date.now()}.min.js`,
 	},
 	module: {
 		rules: [
