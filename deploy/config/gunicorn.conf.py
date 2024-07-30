67bc3bf2d3557c43ead2d15675f43713f6a4987c
deepakdinesh1123 <d.deepakdinesh13@gmail.com>
2024-07-30 17:36:49 +0530

    django settings module variable set in post_fork

--- deploy/config/gunicorn.conf.py
+++ deploy/config/gunicorn.conf.py
@@ -1,9 +1,14 @@
+import os
 import multiprocessing
 
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 
-#https://opentelemetry-python.readthedocs.io/en/latest/examples/fork-process-model/README.html
-from zango.core.monitoring.telemetry import setup_telemetry
+
+# https://opentelemetry-python.readthedocs.io/en/latest/examples/fork-process-model/README.html
 def post_fork(server, worker):
+    project_name = os.environ.get("PROJECT_NAME", "zango_project")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"{project_name}.settings")
+    from zango.core.monitoring.telemetry import setup_telemetry
+
     setup_telemetry(add_django_instrumentation=True)
