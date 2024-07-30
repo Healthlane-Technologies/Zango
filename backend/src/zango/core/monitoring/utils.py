22d96efab6a1942b66a8815c61596e888ee3c313
deepakdinesh1123 <d.deepakdinesh13@gmail.com>
2024-07-30 18:19:27 +0530

    trace export error fixed

--- backend/src/zango/core/monitoring/utils.py
+++ backend/src/zango/core/monitoring/utils.py
@@ -4,49 +4,62 @@ from opentelemetry.sdk._logs import LoggingHandler
 
 
 def otel_is_enabled():
-    """Returns True if env var OTEL_IS_ENABLED is set to any non 
+    """Returns True if env var OTEL_IS_ENABLED is set to any non
     blank string
     """
     if os.getenv("OTEL_IS_ENABLED", "false").strip() == "true":
         return True
     return False
-    
+
+
 def otel_export_to_otlp():
-    """Returns True if env var OTEL_EXPORT_TO_OTLP is set to any non 
+    """Returns True if env var OTEL_EXPORT_TO_OTLP is set to any non
     blank string
     """
-    return bool(os.getenv("OTEL_EXPORT_TO_OTLP", "").strip())
+    if os.getenv("OTEL_EXPORT_TO_OTLP", "false").strip() == "true":
+        return True
+    return False
+
 
 def otel_otlp_endpoint():
     return str(os.getenv("OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4317"))
 
+
 def otel_otlp_headers():
     return str(os.getenv("OTEL_EXPORTER_OTLP_HEADERS", ""))
+
 
 def otel_otlp_protocol():
     return str(os.getenv("OTEL_EXPORTER_PROTOCOL", ""))
 
+
 def otel_resource_name():
-    return str(os.getenv("OTEL_RESOURCE_NAME", "Zango"))    
+    return str(os.getenv("OTEL_RESOURCE_NAME", "Zango"))
+
 
 def _get_tenant_name():
     from django.db import connection
+
     try:
         tenant_name = connection.tenant.name
-    except Exception as e:        
-        tenant_name = 'FakeTenant'
+    except Exception as e:
+        tenant_name = "FakeTenant"
     return tenant_name
+
 
 def _get_tenant_filename(original_name):
     if original_name.startswith("pluginbase"):
-        result = f"workspaces.{_get_tenant_name()}.{'.'.join(original_name.split('.')[3:])}"
+        result = (
+            f"workspaces.{_get_tenant_name()}.{'.'.join(original_name.split('.')[3:])}"
+        )
     else:
-        result = original_name        
+        result = original_name
     return result
-           
+
+
 def get_loguru_format(record):
-    file_name = _get_tenant_filename(record['name'])
-    
+    file_name = _get_tenant_filename(record["name"])
+
     loguru_format = (
         f"<magenta>{os.getpid()}|</magenta>"
         "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green>|"
@@ -58,8 +71,9 @@ def get_loguru_format(record):
     )
     return loguru_format
 
+
 class LogGuruCompatibleLoggerHandler(LoggingHandler):
-    
+
     def emit(self, record: logging.LogRecord) -> None:
         # The Otel exporter does not handle nested dictionaries. Loguru stores all of
         # the extra log context developers can add on the extra dict. Here unnest
