964c8184db6d9a8227b35f1f58dbfadb50ab9d19
devilsautumn <bhuvnesh875@gmail.com>
2024-08-02 18:17:37 +0530

    revert changes related to postgres

--- deploy/init.sh
+++ deploy/init.sh
@@ -4,36 +4,6 @@ if [ -z "$PLATFORM_DOMAIN_URL" ]; then
     PLATFORM_DOMAIN_URL="localhost"
 fi
 
-python << END
-import sys
-import time
-
-import psycopg2
-
-suggest_unrecoverable_after = 30
-start = time.time()
-
-while True:
-    try:
-        psycopg2.connect(
-            dbname="${POSTGRES_DB}",
-            user="${POSTGRES_USER}",
-            password="${POSTGRES_PASSWORD}",
-            host="${POSTGRES_HOST}",
-            port="${POSTGRES_PORT}",
-        )
-        break
-    except psycopg2.OperationalError as error:
-        sys.stderr.write("Waiting for PostgreSQL to become available...\n")
-
-        if time.time() - start > suggest_unrecoverable_after:
-            sys.stderr.write("  This is taking longer than expected. The following exception may be indicative of an unrecoverable error: '{}'\n".format(error))
-
-    time.sleep(1)
-END
-
->&2 echo 'PostgreSQL is available'
-
 if [ "$ENV" = "dev" ]; then 
     if [ -d "$PROJECT_NAME" ]; then
         echo "restarting existing project"
