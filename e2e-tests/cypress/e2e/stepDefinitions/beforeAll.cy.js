fb3b1fb781af826caf1b0a57547313d9c3a4973f
Madhu <madhu@zelthy.com>
2024-08-02 17:29:48 +0530

    e2e-tests

--- e2e-tests/cypress/e2e/stepDefinitions/beforeAll.cy.js
+++ e2e-tests/cypress/e2e/stepDefinitions/beforeAll.cy.js
@@ -1,7 +1,6 @@
-beforeEach(() => {
+before(() => {
   //Login functionality from command function
   // need to call from backend
-  cy.visit("http://localhost:3000/platform/apps");
   Cypress.on("uncaught:exception", (err, runnable) => {
     // returning false here prevents Cypress from
     // failing the test
