f437c6a324e64e336d8804767153911d6950551e
Harsh Shah <shahharsh176@gmail.com>
2024-08-02 16:24:09 +0530

    fix version

--- backend/setup.py
+++ backend/setup.py
@@ -8,7 +8,7 @@ REQUIREMENTS_DIR = os.path.join(PROJECT_DIR, "requirements")
 
 README = os.path.join(PROJECT_DIR, "README.md")
 
-PLATFORM_VERSION = "0.3.0-rc.1"
+PLATFORM_VERSION = "0.3.0rc1"
 
 
 def get_requirements(env):
--- backend/src/zango/__init__.py
+++ backend/src/zango/__init__.py
@@ -1,3 +1,3 @@
 from zango.core import internal_requests
 
-__version__ = "0.3.0-rc.1"
+__version__ = "0.3.0rc1"
