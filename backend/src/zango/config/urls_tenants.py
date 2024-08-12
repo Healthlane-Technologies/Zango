7532cf601a86ea8284d01213c78b0a810058f35c
deepakdinesh1123 <d.deepakdinesh13@gmail.com>
2024-08-12 18:05:34 +0530

    handlers removed

--- backend/src/zango/config/urls_tenants.py
+++ backend/src/zango/config/urls_tenants.py
@@ -20,7 +20,3 @@ if settings.DEBUG:
 urlpatterns += [
     re_path(r"^((?:[\w\-:.,]+/)*)$", include("zango.apps.dynamic_models.urls")),
 ]
-
-# handler404 = "zango.core.generic_views.error.handler_404"
-# handler500 = "zango.core.generic_views.error.handler_500"
-# handler403 = "zango.core.generic_views.error.handler_403"
