cd76cb3d9ec63ba7f51b70168a5acd2f976a1734
prem056627 <prem@zelthy.com>
2024-08-12 18:35:50 +0530

    ADD : Task History added

--- frontend/src/pages/appTaskManagement/components/AppTaskManagement/index.js
+++ frontend/src/pages/appTaskManagement/components/AppTaskManagement/index.js
@@ -14,6 +14,7 @@ import AppTable from '../AppTable';
 import RemoveAllPoliciesModal from '../Modals/RemoveAllPoliciesModal';
 import UpdateTaskModal from '../Modals/UpdateTaskModal';
 import SyncTask from '../SyncTask';
+import TaskHistoryModal from '../AppTable/TaskHistoryModal/Index';
 
 export default function AppTaskManagement() {
 	let { appId } = useParams();
@@ -101,6 +102,7 @@ export default function AppTaskManagement() {
 			</div>
 			<UpdateTaskModal />
 			<RemoveAllPoliciesModal />
+			<TaskHistoryModal/>
 		</>
 	);
 }
