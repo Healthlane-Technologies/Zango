1cee751baf934fe1ea350e6fe2bd9213b46de691
deepakdinesh1123 <d.deepakdinesh13@gmail.com>
2024-08-29 15:29:38 +0530

    release api fixes

--- backend/src/zango/api/platform/releases/v1/serializers.py
+++ backend/src/zango/api/platform/releases/v1/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from zango.apps.release.models import AppRelease
+from zango.core.utils import get_datetime_str_in_tenant_timezone
 
 
 class AppReleaseSerializer(serializers.ModelSerializer):
+    created_at = serializers.SerializerMethodField()
+
     class Meta:
         model = AppRelease
         fields = [
@@ -11,5 +14,11 @@ class AppReleaseSerializer(serializers.ModelSerializer):
             "version",
             "description",
             "status",
-            "release_result",
+            "last_git_hash",
+            "created_at",
         ]
+
+    def get_created_at(self, obj):
+        return get_datetime_str_in_tenant_timezone(
+            obj.created_at, self.context["tenant"]
+        )
