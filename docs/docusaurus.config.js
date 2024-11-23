262f077f6c99d60acd1a4b17e2defcd3ec985fab
Bhuvnesh <bhuvnesh@zelthy.com>
2024-11-23 14:45:41 +0530

    docs version 2 (#415)
    * docs v2
    
    * fixes
    
    * added static files
    
    * added static in gitignore
    
    * removed path values
    
    * final fix

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
+const zdocConfig = require("./zdocs.config.json");
 
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
@@ -7,11 +8,11 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Zango Docs",
-  tagline: "Next-Gen HealthCare App Development Platform",
+  tagline: "Next-Gen Business App Development Platform",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://zelthystatichosting.s3.ap-south-1.amazonaws.com",
+  url: "https://www.zango.dev/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -19,9 +20,9 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "zelthy", // Usually your GitHub org/user name.
-  projectName: "zelthy_docs", // Usually your repo name.
+  projectName: "zango_docs", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
@@ -48,13 +49,15 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/Healthlane-Technologies/zelthy3/tree/documentation/docs",
+            "https://github.com/Healthlane-Technologies/zango/tree/documentation/docs",
+          path: "./docs", // path to your docs folder
+          routeBasePath: "/", //path/word that you want to set
         },
         blog: {
           showReadingTime: true,
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/Healthlane-Technologies/zelthy3/tree/documentation/docs",
+            "https://github.com/Healthlane-Technologies/zango/tree/documentation/docs",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -69,11 +72,13 @@ const config = {
       // Replace with your project's social card
       image: "img/zelthy-social-card.jpg",
       navbar: {
-        title: "Zango Docs",
+        // title: "Zelthy Docs",
         logo: {
           alt: "Zango Logo",
-          src: "img/zelthy_logo.png",
-          href: '/docs/documentation/introduction'
+          src: "img/zango_logo_blk_1.png",
+          srcDark: "img/zango_logo.svg",
+          href: "/core/introduction",
+          style: { width: '100px', height: 'auto',  display: 'flex', alignItems: 'center', paddingTop: '5px' } // Adjust width as needed
         },
         items: [
           {
@@ -82,31 +87,9 @@ const config = {
             position: "left",
             label: "Docs",
           },
-          {
-            label: "Packages", 
-            position: "left",
-            type: "dropdown",
-            items: [
-                {
-                  label: "Login",
-                  to: "/login"
-                },
-                {
-                  label: "Frame",
-                  to: "/frame"
-                },
-                {
-                  label: "Crud",
-                  to: "/crud"
-                },
-                {
-                  label: "Workflow",
-                  to: "/workflow"
-                }
-              ]
-          },
+          ...zdocConfig?.navbar?.items,
           // {
-          //   href: "https://github.com/Healthlane-Technologies/zelthy3",
+          //   href: "https://github.com/Healthlane-Technologies/zango/",
           //   position: "right",
           //   className: "header-github-link",
           // },
@@ -154,7 +137,7 @@ const config = {
           //   title: "More",
           //   items: [
           //     {
-          //       label: "Zango",
+          //       label: "Zelthy",
           //       href: "https://zelthy.com",
           //     },
           //     {
@@ -171,21 +154,58 @@ const config = {
         darkTheme: darkCodeTheme,
       },
       zoom: {
-        selector: '.markdown img',
+        selector: ".markdown img",
         background: {
-          light: 'rgb(255, 255, 255)',
-          dark: 'rgb(50, 50, 50)'
+          light: "rgb(255, 255, 255)",
+          dark: "rgb(50, 50, 50)",
         },
         config: {
           // options you can specify via https://github.com/francoischalifour/medium-zoom#usage
-          margin: 36
-        }
-      }
+          margin: 36,
+        },
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "YZ5AZ15C2L",
+
+        // Public API key: it is safe to commit it
+        apiKey: "669aeed650b1db0aae9d3d2c4508157e",
+
+        indexName: "zcore",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: "external\\.com|domain\\.com",
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        // replaceSearchResultPathname: {
+        //   from: "/", // or as RegExp: /\/docs\//
+        //   to: "/",
+        // },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search",
+
+        //... other Algolia params
+      },
     }),
 
   plugins: [
-    require.resolve("@cmfcmf/docusaurus-search-local"),
-    'docusaurus-plugin-image-zoom'
+    // require.resolve("@cmfcmf/docusaurus-search-local"),
+    "docusaurus-plugin-image-zoom",
+    // [
+    //   "vercel-analytics",
+    //   {
+    //     debug: true,
+    //     mode: "auto",
+    //   },
+    // ],
+    ...zdocConfig.plugins,
   ],
 };
 
