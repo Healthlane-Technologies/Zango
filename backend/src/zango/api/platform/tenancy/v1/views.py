8d4c140e8cb9ee34b4a7521badb0d83f503ae529
deepakdinesh1123 <52698821+deepakdinesh1123@users.noreply.github.com>
2025-02-14 19:10:36 +0530

    release workflow improved (#443)
    * release workflow improved
    
    * is_update_allowed function fix

--- backend/src/zango/api/platform/tenancy/v1/views.py
+++ backend/src/zango/api/platform/tenancy/v1/views.py
@@ -12,7 +12,6 @@ from zango.apps.dynamic_models.workspace.base import Workspace
 from zango.apps.permissions.models import PolicyModel
 from zango.apps.shared.tenancy.models import TenantModel, ThemesModel
 from zango.apps.shared.tenancy.utils import DATEFORMAT, DATETIMEFORMAT, TIMEZONES
-from zango.cli.git_setup import git_setup
 from zango.core.api import (
     ZangoGenericPlatformAPIView,
     get_api_response,
@@ -177,9 +176,6 @@ class AppDetailViewAPIV1(ZangoGenericPlatformAPIView):
     def put(self, request, *args, **kwargs):
         try:
             obj = self.get_obj(**kwargs)
-            old_git_config = (
-                obj.extra_config.get("git_config", {}) if obj.extra_config else {}
-            )
             serializer = TenantSerializerModel(
                 instance=obj,
                 data=request.data,
@@ -190,32 +186,6 @@ class AppDetailViewAPIV1(ZangoGenericPlatformAPIView):
                 serializer.save()
                 success = True
                 status_code = 200
-                if serializer.data.get("extra_config", None):
-                    new_git_config = serializer.data["extra_config"].get(
-                        "git_config", {}
-                    )
-
-                    new_repo_url = new_git_config.get("repo_url")
-                    old_repo_url = old_git_config.get("repo_url")
-
-                    if new_repo_url and (
-                        not old_git_config or new_repo_url != old_repo_url
-                    ):
-                        git_setup(
-                            [
-                                obj.name,
-                                "--git_repo_url",
-                                new_repo_url,
-                                "--dev_branch",
-                                self.get_branch(new_git_config, "dev", "development"),
-                                "--staging_branch",
-                                self.get_branch(new_git_config, "staging", "staging"),
-                                "--prod_branch",
-                                self.get_branch(new_git_config, "prod", "main"),
-                                "--initialize",
-                            ],
-                            standalone_mode=False,
-                        )
 
                 result = {
                     "message": "App Settings Updated Successfully",
