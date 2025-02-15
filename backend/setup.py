115a8110f97edfd6c6a23142371aee4e7d2db9c9
deepakdinesh1123 <52698821+deepakdinesh1123@users.noreply.github.com>
2025-02-15 11:55:18 +0530

    fixed git url parsing in release workflow (#450)
    * fixed git url parsing in release workflow
    
    * update url parsing

--- backend/setup.py
+++ backend/setup.py
@@ -8,7 +8,7 @@ REQUIREMENTS_DIR = os.path.join(PROJECT_DIR, "requirements")
 
 README = os.path.join(PROJECT_DIR, "README.md")
 
-PLATFORM_VERSION = "0.4.2"
+PLATFORM_VERSION = "0.4.3"
 
 
 def get_requirements(env):
