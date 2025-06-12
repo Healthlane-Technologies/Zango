7d963c311629124d1e6b8be667efc62b13e22ef4
deepakdinesh1123 <d.deepakdinesh13@gmail.com>
2025-06-12 11:05:17 +0530

    add healthcheck task

--- backend/src/zango/apps/tasks/tasks.py
+++ backend/src/zango/apps/tasks/tasks.py
@@ -4,3 +4,14 @@ from celery import shared_task
 @shared_task
 def health_check_periodic_task():
     return "Celery beat is alive"
+
+
+@shared_task
+def health_check():
+    import requests
+
+    from django.conf import settings
+
+    resp = requests.get(settings.HEALTH_CHECK_URL)
+    resp.raise_for_status()
+    return {"result": "success"}
