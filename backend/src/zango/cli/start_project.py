fc3e7c865daa4252deb1c795eaa181e110e6ef76
Nandini Gattani <nandini.gattani09@gmail.com>
2025-07-04 17:00:48 +0530

    changes done, comment removed

--- backend/src/zango/cli/start_project.py
+++ backend/src/zango/cli/start_project.py
@@ -98,9 +98,7 @@ def create_project(
     project_template_path = os.path.join(
         os.path.dirname(zango.cli.__file__), "project_template"
     )
-    # command = f"{command} --template '{str(project_template_path)}'"
     command = f'{command} --template "{project_template_path}"'
-
     subprocess.run(command, shell=True, check=True)
     env_keys = {
         "POSTGRES_DB": db_name,
