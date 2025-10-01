6916ef35e22ceef0dec33b00c1d251f7cecc0487
deepakdinesh1123 <d.deepakdinesh13@gmail.com>
2025-10-01 10:47:54 +0530

    fix edit user role form and remove password policy from response

--- backend/src/zango/api/app_auth/profile/v1/views.py
+++ backend/src/zango/api/app_auth/profile/v1/views.py
@@ -4,7 +4,7 @@ from zango.core.api import (
     ZangoGenericAppAPIView,
     get_api_response,
 )
-from zango.core.utils import get_auth_priority, get_current_role
+from zango.core.utils import get_current_role
 
 from .serializers import ProfileSerializer
 
@@ -17,14 +17,10 @@ class ProfileViewAPIV1(ZangoGenericAppAPIView):
         token_serializer = AppUserAuthTokenSerializer(
             tokens, many=True, context={"request": request, "tenant": request.tenant}
         )
-        password_policy = get_auth_priority(
-            policy="password_policy", user=request.user, request=request
-        )
         role = get_current_role()
         response = {
             "message": "success",
             "profile_data": serializer.data,
-            "password_policy": password_policy,
             "should_set_password": request.user.has_usable_password() is False,
             "current_role": {
                 "id": role.id,
