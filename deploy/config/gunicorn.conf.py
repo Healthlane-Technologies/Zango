5ab8fbd693c72fca20979a9a3c40f1fc973e80de
Harsh Shah <harsh.shah@zelthy.com>
2025-11-05 13:03:15 +0530

    add: ECS changes & nested path support for modules (#525)

--- deploy/config/gunicorn.conf.py
+++ deploy/config/gunicorn.conf.py
@@ -3,7 +3,8 @@ import os
 
 
 bind = "0.0.0.0:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
+
+workers = int(os.environ.get("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2 + 1))
 
 
 # https://opentelemetry-python.readthedocs.io/en/latest/examples/fork-process-model/README.html
