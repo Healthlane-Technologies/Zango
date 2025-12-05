85ae738933ec1118547bea7f5ae4593e2e593fa0
Harsh Shah <shahharsh176@gmail.com>
2025-12-05 22:10:29 +0530

    remove build and revert version

--- backend/setup.py
+++ backend/setup.py
@@ -7,7 +7,7 @@ PROJECT_DIR = os.path.dirname(__file__)
 REQUIREMENTS_DIR = os.path.join(PROJECT_DIR, "requirements")
 README = os.path.join(PROJECT_DIR, "README.md")
 
-PLATFORM_VERSION = "0.7.1"
+PLATFORM_VERSION = "0.6.2"
 
 
 def get_requirements(env):
